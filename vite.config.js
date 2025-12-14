14e88ec6fc2eabb193766e81288647666403f83a
lafond <macklafonddeboumas@gmail.com>
2025-12-14 19:57:45 +0100

    Modification du fichier vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.coingecko.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+
   build: {
     outDir: "build",
     assetsDir: "assets2",
